e0b70b006bd87e7db235161a2915f10bd63c3474
Carolyn Isahack <carolynisahack@gmail.com>
2021-07-01 09:57:47 -0400

    Testing update

--- test/pagination.spec.js
+++ test/pagination.spec.js
@@ -1,9 +1,9 @@
-const { getIndex } = require("../pagination");
+const { reqIndex } = require("../pagination");
 
-test("GetIndex returns 0 for id=1", () => {
-  expect(getIndex("id", 1)).toBe(0);
+test("reqIndex returns 0 for id=1", () => {
+  expect(reqIndex("id", 1)).toBe(0);
 });
 
-test("GetIndex returns 0 for name=my-app-001", () => {
-  expect(getIndex("name", "my-app-001")).toBe(0);
+test("reqIndex returns 0 for name=my-app-001", () => {
+  expect(reqIndex("name", "my-app-001")).toBe(0);
 });
